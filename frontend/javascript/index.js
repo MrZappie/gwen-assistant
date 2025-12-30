d1c979af3ec7d0072fe899d12aba876b8f662173
Abhinav-Krishnan-10 <abhinavkrish3@gmail.com>
2025-12-30 11:09:07 +0530

    initial commit

---
+++ frontend/javascript/index.js
@@ -0,0 +1,6 @@
+const selectBtn = document.querySelector('.select-project-btn');
+
+selectBtn.addEventListener('click', () => {
+    // Opens home.html in a new window/tab
+    window.open('home.html', '_blank');
+});
